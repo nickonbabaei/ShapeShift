ed7f8bacdb41b5e44716af57a1d9c663bb52c1a9
Nickon Babaei <babaein1@gmail.com>
2023-03-28 11:08:00 -0300

    ingredientController created

---
+++ controllers/IngredientController.js
@@ -0,0 +1,68 @@
+const { Ingredient } = require('../models')
+
+const getAllIngredients = async (req, res) => {
+    try {
+        const ingredients = await Ingredient.findAll()
+        res.send(ingredients)
+    } catch (error) {
+        throw error
+    }
+}
+
+const getIngredient = async (req, res) => {
+    try {
+        const id = req.params.ingredient_id
+        const ingredient = await Ingredient.findByPk(id)
+        res.send(ingredient)
+    } catch (error) {
+        throw error
+    }
+}
+
+const createIngredient = async (req, res) => {
+    try {
+        let ingredientBody = {
+            ...req.body
+        }
+        let ing = await Ingredient.create(ingredientBody)
+        res.send(ing)
+    } catch (error) {
+        throw error
+    }
+}
+
+const deleteIngredient = async (req, res) => {
+    try {
+        const id = req.params.ingredient_id
+        await Ingredient.destroy(id)
+        res.send({'message': `Ingredient with ID:${id} was deleted`})
+    } catch (error) {
+        throw error
+    }
+    
+}
+
+const updateIngredient = async (req, res) => {
+    try {
+        const id = parseInt(req.params.ingredient_id)
+        const updatedIngredient = await Ingredient.update(req.body, {
+            where: { id: id },
+            returning: true
+        })
+        res.send(updatedIngredient)
+    } catch (error) {
+        throw error
+    }
+}
+
+
+
+
+module.exports = {
+    getAllIngredients,
+    getIngredient,
+    deleteIngredient,
+    createIngredient,
+    updateIngredient
+
+}
