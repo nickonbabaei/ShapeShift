1cd86b999ef9f3e7bffb1da93015860577cc2029
Nickon Babaei <babaein1@gmail.com>
2023-03-30 03:07:16 -0300

    login page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,54 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import {Add} from '@mui/icons-material';
+import Nav from './components/Nav'
+// import Home from './components/Home'
+// import About from './components/About'
+import Login from './pages/Login'
+// import Register from './components/Register';
+import { CheckSession } from './services/Auth';
+import { useState, useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom'
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  let navigate = useNavigate()
+
+  const handleLogOut = () => {
+    setUser(null)
+    localStorage.clear()
+    navigate('/')
+  }
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+
+
   return (
     <div>
-    
+    <Nav
+      user={user}
+      handleLogOut={handleLogOut}
+    />
+    <div className="App">
+      <main className=''>
+        <Routes>
+          {/* <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} /> */}
+          <Route path='/' element={<Login setUser={setUser}/>} />
+          {/* <Route path='/register' element={<Register />}></Route> */}
+        </Routes>
+      </main>
     </div>
+  </div>
   );
 }
 
---
+++ client/src/pages/SetGoal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SetGoal = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default SetGoal
