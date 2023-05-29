05ed2fc7c58ce60cc31914dccf675a60a5a2733e
Nickon Babaei <babaein1@gmail.com>
2023-05-29 14:43:19 -0300

    redesigned goalpage

--- client/src/pages/GoalPage.js
+++ client/src/pages/GoalPage.js
@@ -21,7 +21,7 @@ const GoalPage = ({ user, handleLogout }) => {
         await axios.put(`http://localhost:3001/api/goal/update/${goalInfo.Goal.id}`, editGoal)
         toggleOpen()
         getGoalInfo()
-       
+
     }
 
 
@@ -37,17 +37,66 @@ const GoalPage = ({ user, handleLogout }) => {
     }, [])
 
     return goalInfo && (
-        <section class="bg-sky-100 text-black">
+        <section class="bg-sky-100 text-black h-screen">
             <Nav handleLogout={handleLogout} />
             <div className='flex justify-center'> <img src={ShapeShiftLogo} style={{ borderRadius: '8px', width: '28rem', height: '12rem' }} /> </div>
-            <div
+            <div class="mx-auto max-w-lg text-center">
+                <h2 class="text-2xl font-bold sm:text-4xl"> {goalInfo.username.split(' ')[0].charAt(0).toUpperCase() + goalInfo.username.slice(1).split(' ')[0]}'s Information and Goal</h2>
+            </div>
+            <div className='mx-auto max-w-screen-sm w-5/6 flex flex-col border-2 border-black bg-white px-4 shadow-lg'>
+                <div className='flex justify-between border-b-2 py-2'>
+                    <h2 class="text-md font-semibold text-black">Age:</h2>
+                    <h2 class="text-md text-black">{goalInfo.Goal.age}</h2>
+                </div>
+                <div className='flex justify-between border-b-2 py-2'>
+                    <h2 class="text-md font-semibold text-black">Height:</h2>
+                    <h2 class="text-md text-black">{goalInfo.Goal.height}</h2>
+                </div>
+                <div className='flex justify-between border-b-2 py-2'>
+                    <h2 class="text-md font-semibold text-black">Weight:</h2>
+                    <h2 class="text-md text-black">{goalInfo.Goal.weight}</h2>
+                </div>
+                <div className='flex justify-between border-b-2 py-2'>
+                    <h2 class="text-md font-semibold text-black">Sex:</h2>
+                    <h2 class="text-md text-black">{goalInfo.Goal.sex.charAt(0).toUpperCase() + goalInfo.Goal.sex.slice(1)}</h2>
+                </div>
+                <div className='justify-between border-b-2 py-2 sm:flex'>
+                    <h2 class="text-md font-semibold text-black">Activity Level:</h2>
+                    <p class="mt-1 text-sm text-black">
+                            {goalInfo.Goal.activity === '1.2' ? 'Sedentary (little or no exercise)'
+                                : goalInfo.Goal.activity === '1.375' ? 'Lightly active (light exercise/sports 1-3 days/week)'
+                                    : goalInfo.Goal.activity === '1.55' ? 'Moderately active (moderate exercise/sports 3-5 days/week)'
+                                        : goalInfo.Goal.activity === '1.725' ? 'Very active (hard exercise/sports 6-7 days a week)'
+                                            : goalInfo.Goal.activity === '1.9' ? 'Extra active (very hard exercise/sports & a physical job)'
+                                                : ''
+                            }
+                        </p>
+                </div>
+                <div className='justify-between py-2 sm:flex'>
+                    <h2 class="text-md font-semibold text-black">Goal:</h2>
+                    <p class="mt-1 text-sm text-black">
+                            {goalInfo.Goal.description === '600' ? 'Dirty bulk (gain weight/muscle with no regards to fat gain)'
+                                : goalInfo.Goal.description === '300' ? 'Lean bulk (gain weight/muscle while putting on minimal fat)'
+                                    : goalInfo.Goal.description === '0' ? 'Maintain your current weight'
+                                        : goalInfo.Goal.description === '-300' ? 'Cut weight (lose weight while losing minimum muscle)'
+                                            : goalInfo.Goal.description === '-600' ? 'Lose weight (lose weight with no regards to muscle loss)'
+                                                : ''
+                            }
+                        </p>
+                </div>
+                
+
+
+
+            </div>
+            {/* <div
                 class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
             >
                 <div class="mx-auto max-w-lg text-center pb-6">
                     <h2 class="text-3xl font-bold sm:text-4xl"> {goalInfo.username.split(' ')[0].charAt(0).toUpperCase()+goalInfo.username.slice(1).split(' ')[0]}'s Information and Goal</h2>
                 </div>
 
-                <div class="mt-8 grid grid-cols-2 gap-8 md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-3">
+                <div class="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-3">
                 <a
                         class="block rounded-xl border border-gray-800 p-8 shadow-xl transition "
                         href="/services/digital-campaigns"
@@ -92,7 +141,7 @@ const GoalPage = ({ user, handleLogout }) => {
                     </a>
 
                     <a
-                        class="flex justify-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition  lg:row-start-3"
+                        class="flex justify-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition"
 
                     >
 
@@ -101,7 +150,7 @@ const GoalPage = ({ user, handleLogout }) => {
                     </a>
 
                     <a
-                        class="flex justify-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition  lg:row-start-3"
+                        class="flex justify-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition"
 
                     >
 
@@ -110,7 +159,7 @@ const GoalPage = ({ user, handleLogout }) => {
                     </a>
 
                     <a
-                        class="flex justify-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition  lg:row-start-3"
+                        class="flex justify-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition"
                     >
                         <h2 class="mt-4 text-xl font-bold text-black">{goalInfo.Goal.sex.charAt(0).toUpperCase() + goalInfo.Goal.sex.slice(1)}</h2>
                     </a>
@@ -130,10 +179,10 @@ const GoalPage = ({ user, handleLogout }) => {
                         Update Info/Goal
                     </button>
                 </div>
-            </div>
-            {open && <EditGoalForm toggleOpen={toggleOpen} open={open} editGoal={editGoal} handleGoalChange={handleGoalChange} handleGoalSubmit={handleGoalSubmit}/>}
-                
-            
+            </div> */}
+            {open && <EditGoalForm toggleOpen={toggleOpen} open={open} editGoal={editGoal} handleGoalChange={handleGoalChange} handleGoalSubmit={handleGoalSubmit} />}
+
+
 
         </section>
 
