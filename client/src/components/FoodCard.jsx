99e57ccbeaec4c11949d96ab873915c224ba3292
Nickon Babaei <babaein1@gmail.com>
2023-06-08 17:07:15 -0300

    rearranged foodCard code

--- client/src/components/FoodCard.jsx
+++ client/src/components/FoodCard.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const FoodCard = ({ result, getSpecificFood}) => {
+const FoodCard = ({ result, getSpecificFood }) => {
     return (
         <ol className='flex w-full'>
             <ul class="w-full bg-white rounded-lg shadow-md">
                 <NavLink onClick={() => getSpecificFood(result)}>
                     <li class="px-4 py-2 border-b border-gray-300 hover:bg-gray-100">
                         <h4 className="text-gray-800 font-lg font-bold">{result.description}</h4>
-                        <p className="text-base text-gray-500 dark:text-gray-400">{result?.brandName ? <p>{Math.round(result?.foodNutrients[3]?.value)} cal, {result?.brandName}</p> : <p>{Math.round(result?.foodNutrients[3]?.value)} cal, GENERIC </p>}</p>
+                        <p className="text-base text-gray-500 dark:text-gray-400">
+                            {
+                                result?.brandName ?
+                                    <p>{Math.round(result?.foodNutrients[3]?.value)} cal, {result?.brandName}</p>
+                                    :
+                                    <p>{Math.round(result?.foodNutrients[3]?.value)} cal, GENERIC </p>
+                            }
+                        </p>
                     </li>
                 </NavLink>
             </ul>
         </ol>
     )
 }
-
-
 
 export default FoodCard
