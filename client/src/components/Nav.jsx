b4178f43ad4898f69fa32110378bea0b688b514e
Nickon Babaei <babaein1@gmail.com>
2023-06-16 16:59:14 -0300

    restructed nav bar code

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import Logo from '../images/logo.png'
+import header from '../images/header.png'
 
 
 const Nav = ({ handleLogout, page }) => {
@@ -12,9 +13,12 @@ const Nav = ({ handleLogout, page }) => {
     }
 
     return (
-        <div className="sm:mx-auto flex h-14 justify-between items-center pl-3 md:gap-8 md:px-8">
+        
 
-            <div className="md:hidden">
+        
+        <div className="flex h-14 justify-between items-center px-4 md:gap-8 md:px-8">
+
+            <div className="md:hidden pl-1">
                 <button
                     onClick={handleDropDown}
                     className="rounded bg-blur p-2 text-gray-600 transition hover:text-gray-600"
@@ -44,30 +48,33 @@ const Nav = ({ handleLogout, page }) => {
                 </div>
             </div>
 
-            <div className='md:hidden pl-10'>
+            <div className='md:hidden pl-8'>
                 <h2>{page}</h2>
             </div>
             <div className='md:hidden'>
                 <img src={Logo} className='w-2/3 mx-auto' />
             </div>
 
-            <NavLink className="hidden md:block text-sky-600 hover:text-white" to="/">
-                <h1>Home</h1>
-            </NavLink>
-            <div className="hidden md:flex flex-1 items-center justify-between gap-6">
+            <div className='hidden md:flex'>
+                <NavLink className="pr-4 text-sky-600 hover:text-white" to="/">
+                    <h1>Home</h1>
+                </NavLink>
+
                 <NavLink className="text-sky-600 transition hover:text-white" to='/goalpage'>
                     Details
                 </NavLink>
-
-                <NavLink
-                    className="block rounded-md bg-sky-600 px-5 py-2.5 text-sm font-medium text-white transition border hover:bg-transparent hover:border-sky-600 hover:text-sky-600"
-                    onClick={handleLogout}
-                    to="/"
-                >
-                    Sign Out
-                </NavLink>
             </div>
+
+            <NavLink
+                className="hidden md:block rounded-md bg-sky-600 px-5 py-2.5 text-sm font-medium text-white transition border hover:bg-transparent hover:border-sky-600 hover:text-sky-600"
+                onClick={handleLogout}
+                to="/"
+            >
+                Sign Out
+            </NavLink>
+
         </div>
+       
     );
 };
 
