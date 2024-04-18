59c124f6f27b0749180fc086df9796c9b9301d87
Nickon Babaei <babaein1@gmail.com>
2024-04-18 10:46:07 -0400

    formatting changes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -48,10 +48,7 @@ const Login = ({ setUser }) => {
                     </p>
 
                     <div>
-                        <label
-                            for="email"
-                            class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600"
-                        >
+                        <label for="email" class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600">
                             <input
                                 type="email"
                                 name="email"
@@ -62,19 +59,14 @@ const Login = ({ setUser }) => {
                                 required
                             />
 
-                            <span
-                                class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs"
-                            >
+                            <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
                                 Email
                             </span>
                         </label>
                     </div>
 
                     <div>
-                        <label
-                            for="password"
-                            class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600"
-                        >
+                        <label for="password" class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600">
                             <input
                                 type="password"
                                 name="password"
@@ -85,18 +77,13 @@ const Login = ({ setUser }) => {
                                 required
                             />
 
-                            <span
-                                class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs"
-                            >
+                            <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
                                 Password
                             </span>
                         </label>
                     </div>
 
-                    <button
-                        type="submit"
-                        className="block w-full rounded-lg bg-sky-600 border border-sky-600 px-5 py-3 text-sm font-medium text-white hover:bg-white hover:text-sky-600"
-                    >
+                    <button type="submit" className="block w-full rounded-lg bg-sky-600 border border-sky-600 px-5 py-3 text-sm font-medium text-white hover:bg-white hover:text-sky-600">
                         Sign in
                     </button>
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -232,10 +232,7 @@ const Register = ({ setUser }) => {
                             </div>
 
                             <div class="col-span-6">
-                                <label
-                                    for="email"
-                                    class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600"
-                                >
+                                <label for="email" class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600">
                                     <input
                                         type="email"
                                         name="email"
@@ -245,19 +242,14 @@ const Register = ({ setUser }) => {
                                         onChange={handleChange}
                                         required
                                     />
-                                    <span
-                                        class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs"
-                                    >
+                                    <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
                                         Email
                                     </span>
                                 </label>
                             </div>
 
                             <div class="col-span-6 sm:col-span-3">
-                                <label
-                                    for="password"
-                                    class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600"
-                                >
+                                <label for="password" class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600">
                                     <input
                                         type="password"
                                         name="password"
@@ -268,19 +260,14 @@ const Register = ({ setUser }) => {
                                         required
                                     />
 
-                                    <span
-                                        class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs"
-                                    >
+                                    <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
                                         Password
                                     </span>
                                 </label>
                             </div>
 
                             <div class="col-span-6 sm:col-span-3">
-                                <label
-                                    for="confirmPassword"
-                                    class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600"
-                                >
+                                <label for="confirmPassword" class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-sky-600">
                                     <input
                                         type="password"
                                         name="confirmPassword"
@@ -291,9 +278,7 @@ const Register = ({ setUser }) => {
                                         required
                                     />
 
-                                    <span
-                                        class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs"
-                                    >
+                                    <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
                                         Confirm password
                                     </span>
                                 </label>
